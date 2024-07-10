8129e79e93c12416e61bb44545dcd4c1e055fd6d
radumusoaie20 <pavelradu.musoaie@ulbsibiu.ro>
2024-07-10 19:09:14 +0300

    Added a category page where you can add and delete categories! In case you add an already existing category, it won't be added twice!

---
+++ ExpenseTracker/Data/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpenseTracker.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category(int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+    }
+}
